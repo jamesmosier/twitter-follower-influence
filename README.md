853cb5e9d5545e4cffef1fceade946db665328c9
James Mosier <jamesdmosier@gmail.com>
2017-11-30 15:12:50 -0500

    initial commit :wave:

---
+++ README.md
@@ -0,0 +1,1 @@
+# twitter-follower-influence
---
+++ index.js
@@ -0,0 +1,101 @@
+const axios = require('axios');
+const _ = require('lodash');
+const request = require('request')
+const jsonfile = require('jsonfile');
+
+const consumer_key = 'YOUR_CONSUMER_KEY';
+const consumer_secret = 'YOU_CONSUMER_SECRET';
+const encode_secret = new Buffer(`${consumer_key}:${consumer_secret}`).toString(
+  'base64'
+);
+
+let oauthHeader = null;
+
+const options = {
+  url: 'https://api.twitter.com/oauth2/token',
+  headers: {
+    Authorization: 'Basic ' + encode_secret,
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+  },
+  body: 'grant_type=client_credentials',
+};
+
+request.post(options, (error, response, body) => {
+  const resp = JSON.parse(body);
+  oauthHeader = resp.access_token;
+
+  getUserFollowers();
+});
+
+
+
+// const tokenPostData = { 'grant_type': 'client_credentials' };
+// const tokenHeaders = {
+//   headers: {
+//     Authorization: 'Basic ' + encode_secret,
+//     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+//   }
+// };
+
+// axios({
+//   method: 'post',
+//   url: 'https://api.twitter.com/oauth2/token',
+//   data: { 'grant_type': 'client_credentials' },
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+//     'Accept': 'application/json; charset=utf-8',
+//     Authorization: 'Basic ' + encode_secret
+//   }
+// })
+//   .then((data) => {
+//     const resp = JSON.parse(data);
+//     console.log(resp);
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//     throw err;
+//   });
+
+let followers = [];
+
+function getUserFollowers(cursor) {
+  const followersApiUrl =
+    `https://api.twitter.com/1.1/followers/list.json?cursor=${cursor ? cursor : -1}&screen_name=jamez14&skip_status=true&include_user_entities=false&count=200`;
+
+  return axios
+    .get(followersApiUrl, {
+      headers: { Authorization: `Bearer ${oauthHeader}` },
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        const nextCursor = response.data.next_cursor;
+        const users = response.data.users;
+
+        const output = _.sortBy(users, u => u.followers_count);
+
+        const usersArr= _.map(output, (user) => {
+          return {
+            handle: user.screen_name,
+            follower_count: user.followers_count
+          };
+        });
+
+        followers = followers.concat(usersArr);
+
+        if (nextCursor !== 0) {
+          getUserFollowers(nextCursor);
+        } else {
+          jsonfile.writeFile('output.json', followers, function (err) {
+            if (err) {
+              console.error(err);
+            }
+          })
+        }
+      } else {
+        console.error(`Request was not sucessful, received a status code of: ${response.status}`)
+      }
+    })
+    .catch((e) => {
+      console.error('Error getting user followers', e);
+    });
+}
